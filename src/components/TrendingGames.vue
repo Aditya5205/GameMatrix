e31e0207bf09cf615e7bb2f7024b9b6db22612c8
Aditya <adityasingh19122002@gmail.com>
2024-03-07 19:37:40 +0530

    Add loading icon while getting games

--- src/components/TrendingGames.vue
+++ src/components/TrendingGames.vue
@@ -1,59 +1,69 @@
 <template>
-  <div
-    class="mx-5 mb-6 text-center text-xl text-red-light sm:text-left sm:text-2xl"
-  >
-    Trending Games
-  </div>
-
-  <div class="mx-5 grid grid-cols-1 gap-x-5 gap-y-7 sm:grid-cols-4">
-    <div v-for="(game_data, index) in trending_games_data" :key="index">
-      <img
-        class="rounded-xl"
-        :src="`${game_data.Image}`"
-        alt="steam_game_image"
-      />
-      <div class="mt-1 flex">
-        <div class="w-3/4 text-white-dark">
-          <div
-            class="cursor-pointer text-wrap text-sm text-red-light hover:text-white-dark sm:text-base"
-            @click="goEmit(game_data.Name)"
-          >
-            {{ game_data.Name }}
+  <div v-if="isApiDone">
+    <div
+      class="mx-5 mb-6 text-center text-xl text-red-light sm:text-left sm:text-2xl"
+    >
+      Trending Games
+    </div>
+    <div class="mx-5 grid grid-cols-1 gap-x-5 gap-y-7 sm:grid-cols-4">
+      <div v-for="(game_data, index) in trending_games_data" :key="index">
+        <img
+          class="rounded-xl"
+          :src="`${game_data.Image}`"
+          alt="steam_game_image"
+        />
+        <div class="mt-1 flex">
+          <div class="w-3/4 text-white-dark">
+            <div
+              class="cursor-pointer text-wrap text-sm text-red-light hover:text-white-dark sm:text-base"
+              @click="goEmit(game_data.Name)"
+            >
+              {{ game_data.Name }}
+            </div>
+            <div class="text-xs sm:text-sm">{{ game_data.Price }}</div>
           </div>
-          <div class="text-xs sm:text-sm">{{ game_data.Price }}</div>
+          <a
+            :href="`${game_data.Steam}`"
+            class="ml-auto flex items-center rounded-full"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              class="w-7 rounded-full bg-white-dark hover:opacity-70 sm:w-9"
+              src="../assets/img/8679449_steam_fill_icon.png"
+              alt="steam_logo"
+              loading="lazy"
+            />
+          </a>
         </div>
-
-        <a
-          :href="`${game_data.Steam}`"
-          class="ml-auto flex items-center rounded-full"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            class="w-7 rounded-full bg-white-dark hover:opacity-70 sm:w-9"
-            src="../assets/img/8679449_steam_fill_icon.png"
-            alt="steam_logo"
-            loading="lazy"
-          />
-        </a>
       </div>
     </div>
+    <div class="h-10"></div>
   </div>
-  <div class="h-10"></div>
+  <loadingScreen v-else />
 </template>
 
 <script setup>
 import axios from "axios";
 import { onBeforeMount, ref } from "vue";
+import loadingScreen from "../components/Loader.vue";
 
 const trending_games_data = ref([]);
+const isApiDone = ref(false);
 
 const receivePayload = async () => {
   const path = "https://game-recommendation-flask.onrender.com/trend";
 
-  return axios.get(path).then((res) => {
-    trending_games_data.value = res.data.payload;
-  });
+  return axios
+    .get(path)
+    .then((res) => {
+      trending_games_data.value = res.data.payload;
+      isApiDone.value = true;
+    })
+    .catch((err) => {
+      console.log("fetching your data");
+      console.log(`error message is ${err}`);
+    });
 };
 
 const emit = defineEmits(["trend_game"]);
